3017acbdcdac8c558963f087f6154b9f3afc2d06
jorda <>
2013-06-21 14:46:52 +0000

    submit jobs per signal

---
+++ TopHiggsAnalysis/submitAllSignaltHAnalysis.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use Time::Local;
+use Getopt::Std;
+
+getopts('p:');
+if($opt_p) {$prefix = $opt_p;}
+else { die "usage: ./submitAllSignaltHAnalysis.pl -p <prefix> ";}
+
+## example: 
+## system("python cmst3_submit_manyfilesperjob.py MC_or_Data MC_Dataset_Name 5 TopHiggsApp 8nh tH isMC?");
+
+print  "submitting signal ...\n";
+
+system("python cmst3_submit_manyfilesperjob.py MC tH125q_blvu_Yt1_H126toWW 5 TopHiggsApp 8nh tH 1");
+#system("python cmst3_submit_manyfilesperjob.py MC  5 TopHiggsApp 8nh tH 1");
+#system("python cmst3_submit_manyfilesperjob.py MC  5 TopHiggsApp 8nh tH 1");
+
+print "\nDONE \n";
+
+
