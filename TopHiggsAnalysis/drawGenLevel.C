1342f1d3b82d687f9999de6c4b70ff021c447c44
jorda <>
2013-06-28 09:55:34 +0000

    added some new plots

--- TopHiggsAnalysis/drawGenLevel.C
+++ TopHiggsAnalysis/drawGenLevel.C
@@ -25,7 +25,10 @@ void drawGenLevel(){
 
   TH1F* hGenHiggsEta = new TH1F("hGenHiggsEta","",40,-5.0,5.0);
   TH1F* hGenTopEta   = new TH1F("hGenTopEta"  ,"",40,-5.0,5.0);
-  /*
+
+  TH1F* hGenForwardQuarkEta = new TH1F("hGenForwardQuarkEta","",40,-5.0,5.0);
+  TH1F* hGenbQuarkEta       = new TH1F("hGenbQuarkEta"      ,"",40,-5.0,5.0);
+
   TCanvas *c1 = new TCanvas("canvas1","canvas1");
   c1->cd();
   hLeptonMinusfromWfromH->SetNormFactor(1.);
@@ -56,7 +59,7 @@ void drawGenLevel(){
   TCanvas *c4 = new TCanvas("canvas4","canvas4");
   c4->cd();
 
-  l2 = new TLegend(0.50,0.6,0.70,0.87,NULL,"brNDC");
+  l2 = new TLegend(0.50,0.6,0.65,0.7,NULL,"brNDC");
   l2->AddEntry(hLeptonMinusfromWfromH,  " lepton(+) from W(+)","l");
   l2->AddEntry(hLeptonPlusfromWfromH ,  " lepton(-) from W(-)","l");
   l2->AddEntry(hLeptonfromWfromT     ,  " lepton(\\pm) from t(\\pm)","l");
@@ -68,15 +71,14 @@ void drawGenLevel(){
   hLeptonPlusfromWfromH->Draw("same");
   hLeptonfromWfromT->Draw("same");
   l2->Draw("same");
-  */
 
-  /*
   TCanvas *c5 = new TCanvas("canvas5","canvas5");
   c5->cd();
   hGenHiggsPt->SetNormFactor(1.);
   hGenHiggsPt->SetLineColor(1);
   hGenHiggsPt->SetLineWidth(2);
-  hGenHiggsPt->GetXaxis()->SetTitle("p^{H}_{T} (GeV)");
+  hGenHiggsPt->SetTitle("Higgs pT");
+  hGenHiggsPt->GetXaxis()->SetTitle("p^{gen}_{T} (GeV)");
   hGenHiggsPt->GetYaxis()->SetTitle("normalized to 1");
   tree->Draw("genHiggsPt>>hGenHiggsPt","genHiggsPt>0.");
 
@@ -85,7 +87,8 @@ void drawGenLevel(){
   hGenTopPt->SetNormFactor(1.);
   hGenTopPt->SetLineColor(2);
   hGenTopPt->SetLineWidth(2);
-  hGenTopPt->GetXaxis()->SetTitle("p^{top}_{T} (GeV)");
+  hGenTopPt->SetTitle("Top pT");
+  hGenTopPt->GetXaxis()->SetTitle("p^{gen}_{T} (GeV)");
   hGenTopPt->GetYaxis()->SetTitle("normalized to 1");
   tree->Draw("genTopPt>>hGenTopPt","genTopPt>0.");
 
@@ -95,22 +98,21 @@ void drawGenLevel(){
   hGenTopPt->Draw();
   hGenHiggsPt->Draw("same");
 
-  l3 = new TLegend(0.50,0.6,0.70,0.87,NULL,"brNDC");
+  l3 = new TLegend(0.50,0.6,0.60,0.7,NULL,"brNDC");
   l3->AddEntry(hGenTopPt  , " Top","l");
   l3->AddEntry(hGenHiggsPt, " Higgs","l");
   l3->SetTextSize(0.03);
   l3->SetFillColor(kWhite);
   l3->SetBorderSize(0);  
-
   l3->Draw("same");
-  */
   
   TCanvas *c8 = new TCanvas("canvas8","canvas8");
   c8->cd();
   hGenHiggsEta->SetNormFactor(1.);
   hGenHiggsEta->SetLineColor(1);
   hGenHiggsEta->SetLineWidth(2);
-  hGenHiggsEta->GetXaxis()->SetTitle("\\eta^{H}");
+  //hGenHiggsEta->SetTitle("Higgs \\eta");
+  hGenHiggsEta->GetXaxis()->SetTitle("\\eta^{gen}");
   hGenHiggsEta->GetYaxis()->SetTitle("normalized to 1");
   tree->Draw("genHiggsEta>>hGenHiggsEta");
 
@@ -119,7 +121,8 @@ void drawGenLevel(){
   hGenTopEta->SetNormFactor(1.);
   hGenTopEta->SetLineColor(2);
   hGenTopEta->SetLineWidth(2);
-  hGenTopEta->GetXaxis()->SetTitle("\\eta^{top}");
+  //hGenTopEta->SetTitle("Top \\eta");
+  hGenTopEta->GetXaxis()->SetTitle("\\eta^{gen}");
   hGenTopEta->GetYaxis()->SetTitle("normalized to 1");
   tree->Draw("genTopEta>>hGenTopEta");
 
@@ -129,7 +132,7 @@ void drawGenLevel(){
   hGenTopEta->Draw();
   hGenHiggsEta->Draw("same");
 
-  l4 = new TLegend(0.50,0.3,0.70,0.57,NULL,"brNDC");
+  l4 = new TLegend(0.5,0.3,0.6,0.4,NULL,"brNDC");
   l4->AddEntry(hGenTopEta  , " Top","l");
   l4->AddEntry(hGenHiggsEta, " Higgs","l");
   l4->SetTextSize(0.03);
@@ -137,6 +140,40 @@ void drawGenLevel(){
   l4->SetBorderSize(0);  
   l4->Draw("same");
 
-}
+  TCanvas *c11 = new TCanvas("canvas11","canvas11");
+  c11->cd();
+  hGenForwardQuarkEta->SetNormFactor(1.);
+  hGenForwardQuarkEta->SetLineWidth(2);
+  hGenForwardQuarkEta->SetLineColor(kOrange+4);
+  //hGenForwardQuarkEta->SetTitle("quark q \\eta");
+  hGenForwardQuarkEta->GetXaxis()->SetTitle("\\eta^{gen}");
+  hGenForwardQuarkEta->GetYaxis()->SetTitle("normalized to 1");
+  tree->Draw("genForwardQuark_Eta>>hGenForwardQuarkEta");
+  
+  TCanvas *c12 = new TCanvas("canvas12","canvas12");
+  c12->cd();
+  hGenbQuarkEta->SetNormFactor(1.);
+  hGenbQuarkEta->SetLineWidth(2);
+  //hGenbQuarkEta->SetTitle("quark b \\eta");
+  hGenbQuarkEta->SetLineColor(kOrange+7);
+  hGenbQuarkEta->GetXaxis()->SetTitle("\\eta^{q}");
+  hGenbQuarkEta->GetYaxis()->SetTitle("normalized to 1");
+  tree->Draw("genbQuark_Eta>>hGenbQuarkEta");
+
+  TCanvas *c13 = new TCanvas("canvas13","canvas13");
+  c13->cd(); 
+  hGenbQuarkEta->Draw();
+  hGenForwardQuarkEta->Draw("same");
+
+  l5 = new TLegend(0.2,0.7,0.3,0.8,NULL,"brNDC");
+  l5->AddEntry(hGenForwardQuarkEta, " q quark","l");
+  l5->AddEntry(hGenbQuarkEta      , " b quark","l");
+  l5->SetTextSize(0.03);
+  l5->SetFillColor(kWhite);
+  l5->SetBorderSize(0);  
+  l5->Draw("same");
+
+
+} 
 
 
